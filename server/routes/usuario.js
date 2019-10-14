f658ebab7fbe2b4af77a0f6b6ad710700278e704
Jordi Nieto <jordi.nieto@ext.aqualogybs.net>
2019-10-14 17:10:09 +0200

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,11 +3,16 @@ const bcrypt = require("bcrypt");
 const _ = require("underscore");
 
 const Usuario = require("../models/usuario");
+const {
+  verificaToken,
+  verificaAdmin_role
+} = require("../middlewares/autenticacion");
+
 const app = express();
 
 const options = { new: true, runValidators: true };
 
-app.get("/usuario", function(req, res) {
+app.get("/usuario", verificaToken, (req, res) => {
   let desde = req.query.desde || 0;
   desde = Number(desde);
 
@@ -34,7 +39,7 @@ app.get("/usuario", function(req, res) {
     });
 });
 
-app.post("/usuario", function(req, res) {
+app.post("/usuario", [verificaToken, verificaAdmin_role], (req, res) => {
   let body = req.body;
 
   let usuario = new Usuario({
@@ -59,7 +64,7 @@ app.post("/usuario", function(req, res) {
   });
 });
 
-app.put("/usuario/:id", function(req, res) {
+app.put("/usuario/:id", [verificaToken, verificaAdmin_role], (req, res) => {
   let id = req.params.id;
   let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -77,7 +82,7 @@ app.put("/usuario/:id", function(req, res) {
   });
 });
 
-app.delete("/usuario/:id", function(req, res) {
+app.delete("/usuario/:id", [verificaToken, verificaAdmin_role], (req, res) => {
   let id = req.params.id;
 
   let cambioEstado = {
