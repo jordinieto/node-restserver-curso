1b8d0bfb3f00d8e05f65bc96dbd71edd6a9dc157
Jordi Nieto <jordi.nieto@ext.aqualogybs.net>
2019-10-14 03:15:27 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ==================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ==================
+// Entorno
+// ==================
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+// ==================
+// Entorno
+// ==================
+
+let urlDB;
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB =
+    "mongodb+srv://jornieto:D5n2bQ6YIZIx3IX6@cluster0-eshek.mongodb.net/cafe";
+}
+process.env.URLDB = urlDB;
