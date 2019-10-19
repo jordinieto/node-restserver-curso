bb1650cc16bd483b0fee15ac5f18f458e387d58b
Jordi Nieto <jordi.nieto@ext.aqualogybs.net>
2019-10-19 23:18:15 +0200

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -29,17 +29,38 @@ let verificaAdmin_role = (req, res, next) => {
   if (usuario.role === "ADMIN_ROLE") {
     next();
   } else {
-      return res.json({
-          ok: false,
-          err: {
-              message: "El usuario no es administrador"
-          }
-      });
+    return res.json({
+      ok: false,
+      err: {
+        message: "El usuario no es administrador"
+      }
+    });
   }
-  
+};
+
+// ==================
+// Vericficar TokenImg
+// ==================
+let verificaTokenImg = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: "Token no válido"
+        }
+      });
+    }
+
+    req.usuario = decoded.usuario;
+    next();
+  });
 };
 
 module.exports = {
   verificaToken,
-  verificaAdmin_role
+  verificaAdmin_role,
+  verificaTokenImg
 };
--- server/models/usuario.js
+++ server/models/usuario.js
@@ -9,36 +9,17 @@ let rolesValidos = {
 };
 
 let usuarioSchema = new Schema({
-  nombre: {
-    type: String,
-    required: [true, "El nombre es necesario"]
-  },
+  nombre: { type: String, required: [true, "El nombre es necesario"] },
   email: {
     type: String,
     unique: true,
-    required: [true, "El correo es necario"]
+    required: [true, "El correo es necesario"]
   },
-  password: {
-    type: String,
-    required: [true, "La contraseña es obligatoria"]
-  },
-  img: {
-    type: String,
-    required: false
-  },
-  role: {
-    type: String,
-    default: "USER_ROLE",
-    enum: rolesValidos
-  },
-  estado: {
-    type: Boolean,
-    default: true
-  },
-  google: {
-    type: Boolean,
-    default: false
-  }
+  password: { type: String, required: [true, "La contraseña es obligatoria"] },
+  img: { type: String, required: false },
+  role: { type: String, default: "USER_ROLE", enum: rolesValidos },
+  estado: { type: Boolean, default: true },
+  google: { type: Boolean, default: false }
 });
 
 usuarioSchema.methods.toJSON = function() {
